1860c6cd62bb60322e8006c7fc975a8a58a4f186
soufianenajim99 <soufianenajim100@gmail.com>
2024-10-10 09:54:15 +0100

    WV2-1 Adding Employee succfully

--- src/main/java/entities/Employee.java
+++ src/main/java/entities/Employee.java
@@ -1,11 +1,129 @@
 package entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name="employees")
-public class Employee {
+public class Employee extends User {
+    @Column(name = "children_number")
+    private int children_number;
+    @Column(name = "leave_balance")
+    private double leaveBalance;
+    @Column(name = "performance_rating")
+    private double performanceRating;
+    @Column(name = "hire_date")
+    private LocalDate hireDate;
+    @Column(name = "social_security_number")
+    private String socialSecurityNumber;
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+    private double salary;
+    @ManyToOne
+    @JoinColumn(name="department_id")
+    private Department department;
+
+    public Employee() {
+    }
+
+    public Employee(String address, String password, String username, Department department, double salary, LocalDate dateOfBirth, String socialSecurityNumber, LocalDate hireDate, double performanceRating, double leaveBalance, int children_number) {
+        super(address, password, username);
+        this.department = department;
+        this.salary = salary;
+        this.dateOfBirth = dateOfBirth;
+        this.socialSecurityNumber = socialSecurityNumber;
+        this.hireDate = hireDate;
+        this.performanceRating = performanceRating;
+        this.leaveBalance = leaveBalance;
+        this.children_number = children_number;
+    }
+    public Employee(String address, String password, String username, double salary, LocalDate dateOfBirth, String socialSecurityNumber, LocalDate hireDate, double performanceRating, double leaveBalance, int children_number) {
+        super(address, password, username);
+        this.salary = salary;
+        this.dateOfBirth = dateOfBirth;
+        this.socialSecurityNumber = socialSecurityNumber;
+        this.hireDate = hireDate;
+        this.performanceRating = performanceRating;
+        this.leaveBalance = leaveBalance;
+        this.children_number = children_number;
+    }
 
 
+
+    public Employee(int id, String username, String password, String address, int children_number, double leaveBalance, double performanceRating, LocalDate hireDate, String socialSecurityNumber, LocalDate dateOfBirth, double salary, Department department) {
+        super(id, username, password, address);
+        this.children_number = children_number;
+        this.leaveBalance = leaveBalance;
+        this.performanceRating = performanceRating;
+        this.hireDate = hireDate;
+        this.socialSecurityNumber = socialSecurityNumber;
+        this.dateOfBirth = dateOfBirth;
+        this.salary = salary;
+        this.department = department;
+    }
+
+    public int getChildren_number() {
+        return children_number;
+    }
+
+    public void setChildren_number(int children_number) {
+        this.children_number = children_number;
+    }
+
+    public double getLeaveBalance() {
+        return leaveBalance;
+    }
+
+    public void setLeaveBalance(double leaveBalance) {
+        this.leaveBalance = leaveBalance;
+    }
+
+    public double getPerformanceRating() {
+        return performanceRating;
+    }
+
+    public void setPerformanceRating(double performanceRating) {
+        this.performanceRating = performanceRating;
+    }
+
+    public LocalDate getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(LocalDate hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public void setSocialSecurityNumber(String socialSecurityNumber) {
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
 }
